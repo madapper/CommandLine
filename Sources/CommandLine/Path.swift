b6eaa03c8e010545813b4477a0b3348f42d07608
Paul Napier <paul.napier@mi9.com.au>
2016-11-30 10:25:15 +1100

    Commandline will now allow any script to be run independent of plug in packages if the corresponding scipt runner is available. i.e. swift, sh, bash, py etc

--- Sources/CommandLine/Path.swift
+++ Sources/CommandLine/Path.swift
@@ -1,10 +1,15 @@
 import Foundation
 
-public struct Path {
+public struct Path: Command {
     public let components:[String]
     public var description: String {
         return components.joined(separator: "/").replacingOccurrences(of: " ", with: "\\ ")
     }
+    
+    public var rawValue: String {
+        return description
+    }
+    
     public init(components: [String]){
         self.components = components
     }
